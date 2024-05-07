6ccf8ce2dad987b0c45d98e18ea1a66b77c01d4f
dd2558 <dd2558@naver.com>
2024-05-07 18:00:08 +0900

    일반회원 가입 완료

--- mycarlong-back/src/main/java/com/mycarlong/mycarlongback/controller/SignupController.java
+++ mycarlong-back/src/main/java/com/mycarlong/mycarlongback/controller/SignupController.java
@@ -1,5 +1,6 @@
 package com.mycarlong.mycarlongback.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,14 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mycarlong.mycarlongback.dto.ApiResponse;
 import com.mycarlong.mycarlongback.dto.SignupRequest;
+import com.mycarlong.mycarlongback.entity.UserEntity;
+import com.mycarlong.mycarlongback.repository.UserRepository;
 
 @RestController
 public class SignupController {
-  @PostMapping("/api/signup")
+  @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/api/signup")
     public ResponseEntity<?> signup(@RequestBody SignupRequest signupRequest) {
-        // 여기서부터는 받은 데이터를 활용하여 회원가입 로직을 수행
-        // 데이터베이스에 새로운 사용자 정보를 저장하고 등등의 작업을 수행
-        System.out.println("singupcontroller도착");
+        // 회원가입 로직 수행
+        UserEntity user = new UserEntity();
+        user.setName(signupRequest.getName());
+        user.setEmail(signupRequest.getEmail());
+        user.setPassword(signupRequest.getPassword());
+        user.setContact(signupRequest.getContact());
+        user.setRole("ROLE_USER");
+
+        // 데이터베이스에 사용자 정보 저장
+        UserEntity savedUser = userRepository.save(user);
+        System.out.println("가입자 = " + savedUser.getName());
         // 회원가입이 성공하면 적절한 응답을 클라이언트에게 전송
         return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse(true, "회원가입이 완료되었습니다."));
     }
