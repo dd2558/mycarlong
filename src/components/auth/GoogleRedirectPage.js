14ff7651742207336ba1da0e24dff4443793da59
dd2558 <dd2558@naver.com>
2024-05-09 12:30:48 +0900

    로그아웃 시작

--- src/components/auth/GoogleRedirectPage.js
+++ src/components/auth/GoogleRedirectPage.js
@@ -1,32 +1,46 @@
-import React, {useEffect} from 'react';
-import {useLocation, useNavigate} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const GoogleRedirectPage = () => {
     const location = useLocation();
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
 
     const handleOAuthGoogle = async (code) => {
         try {
-            // 구글로부터 받아온 code를 서버에 전달하여 구글로 회원가입 & 로그인한다
             const response = await axios.get(`http://localhost:8080/oauth/login/google?code=${code}`);
-            const data = response.data; // 응답 데이터
-            alert("로그인 성공: " + data)
-            navigate("/success");
+            // 응답 데이터에서 성공 여부 확인
+            if (response.status === 200) {
+                // 로그인 성공 시 토큰을 스토리지에 저장
+                // 여기서 토큰을 저장하는 로직 추가
+                alert("로그인 되었습니다");
+                navigate("/success");
+            } else {
+                alert("로그인 실패");
+                navigate("/fail");
+            }
         } catch (error) {
+            alert("로그인 실패");
             navigate("/fail");
+        } finally {
+            setLoading(false); // 로딩 상태 변경
         }
     };
 
     useEffect(() => {
         const searchParams = new URLSearchParams(location.search);
-        const code = searchParams.get('code');  // 구글은 Redirect 시키면서 code를 쿼리 스트링으로 준다.
+        const code = searchParams.get('code');
         const codeTest = encodeURIComponent(code);
         if (codeTest) {
-            alert("CODE = " + codeTest)
+            setLoading(true); // 로딩 상태 변경
             handleOAuthGoogle(codeTest);
         }
     }, [location]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div>
